a9e1d03293b53f21342d17656b0baa822ed2c45b
Shayan <shayanhz.dev@gmail.com>
2026-09-08 23:44:13 +0330

    Adds user login and token generation
    Introduces a `login` method to the `UserService` to enable user authentication.
    
    This method handles:
    - Retrieving user details by email.
    - Verifying the provided password against the stored hash using `PasswordService`.
    - Generating an authentication token via `TokenService` upon successful login.
    - Raising appropriate errors for invalid credentials.

--- app/modules/users/service/user_service.py
+++ app/modules/users/service/user_service.py
@@ -1,8 +1,7 @@
-from app.modules.users.repositories import user_repository
 from app.modules.users.repositories.user_repository import UserRepository
-from app.modules.users.service import password_service
 from app.modules.users.service.password_service import PasswordService
 from app.modules.users.models.user import User
+from app.modules.users.service.token_service import TokenService
 from sqlalchemy.orm import Session
 
 class UserService:
@@ -10,11 +9,13 @@ class UserService:
             self,
             user_repository: UserRepository,
             password_service: PasswordService,
+            token_service: TokenService,
             session: Session
             ):
         
         self.user_repository = user_repository
         self.password_service = password_service
+        self.token_service = token_service
         self.session = session
 
     def register(
@@ -42,3 +43,14 @@ class UserService:
         except Exception:
             self.session.rollback()
             raise
+
+    def login(self, email: str, password: str) -> str:
+        user = self.user_repository.get_by_email(email)
+        if not user:
+            raise ValueError("Invalid email or password.")
+
+        if not self.password_service.verify(password, user.password_hash):
+            raise ValueError("Invalid email or password.")
+
+        return self.token_service.create_token(user_id=user.id)
+    
