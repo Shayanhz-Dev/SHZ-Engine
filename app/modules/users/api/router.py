ed587f9aac57d75c3b1a9d40fa323aecec34fce2
Shayan <shayanhz.dev@gmail.com>
2026-09-09 00:30:03 +0330

    Implements user authentication API endpoint
    Adds a `/login` endpoint to allow users to authenticate with their email and password.
    
    Upon successful authentication, the endpoint returns an access token, enabling subsequent authorized requests. It properly handles invalid credentials by responding with a 401 Unauthorized status and a WWW-Authenticate header.

--- app/modules/users/api/router.py
+++ app/modules/users/api/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.modules.users.api.dependencies import get_user_service
 from app.modules.users.service.user_service import UserService
-from app.modules.users.schemas.user import RegisterRequest, UserResponse
+from app.modules.users.schemas.user import RegisterRequest, UserResponse, TokenResponse, LoginRequest
 
 router = APIRouter(
     prefix="/users",
@@ -29,3 +29,25 @@ def register(
     
     except ValueError as error:
         raise HTTPException(status_code=400, detail=str(error))
+
+@router.post(
+        "/login",
+        response_model=TokenResponse,)
+def login(
+    data: LoginRequest,
+    service: UserService = Depends(get_user_service),
+):
+    try:
+        access_token = service.login(
+            email=data.email,
+            password=data.password
+        )
+        return {
+            "access_token": access_token,
+            "token_type": "bearer"
+        }
+    except ValueError as error:
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"})
