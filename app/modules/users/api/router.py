d9585e2999dd4f369f0b205eff514df93881a390
Shayan <shayanhz.dev@gmail.com>
2026-09-09 12:56:17 +0330

    Adds endpoint to fetch current user details
    Introduces a `/me` endpoint, allowing authenticated users to retrieve their own profile information. Updates the user registration process to accept a `full_name` instead of a `username` for improved user identification.

--- app/modules/users/api/router.py
+++ app/modules/users/api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 
-from app.modules.users.api.dependencies import get_user_service
+from app.modules.users.api.dependencies import get_user_service , get_current_user
 from app.modules.users.service.user_service import UserService
 from app.modules.users.schemas.user import RegisterRequest, UserResponse, TokenResponse, LoginRequest
+from app.modules.users.models.user import User
 
 router = APIRouter(
     prefix="/users",
@@ -21,7 +22,7 @@ def register(
 ):
     try:
         user = user_service.register(
-            username=data.username,
+            full_name=data.full_name,
             email=data.email,
             password=data.password
             )
@@ -51,3 +52,13 @@ def login(
             status.HTTP_401_UNAUTHORIZED,
             detail=str(error),
             headers={"WWW-Authenticate": "Bearer"})
+
+@router.get(
+    "/me",
+    response_model=UserResponse,
+    status_code=status.HTTP_200_OK
+)
+def get_me(
+    current_user: User = Depends(get_current_user),
+):
+    return current_user
