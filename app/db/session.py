fbb68e69a110a49185928f530d133adf02384f93
Shayan <shayanhz.dev@gmail.com>
2026-09-09 12:55:52 +0330

    Adds `get_db` dependency for sessions
    Introduces a `get_db` function to provide a database session.
    This function ensures proper session lifecycle management by automatically closing the session after use, preventing resource leaks.
    It is designed to be used as a dependency injector, simplifying database interaction in application endpoints.

--- app/db/session.py
+++ app/db/session.py
@@ -2,3 +2,10 @@ from sqlalchemy.orm import sessionmaker
 from app.db.engine import engine
 
 sessionlocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
